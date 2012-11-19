8ea06d39a6b6c58af393e5695c11abe445e1c981
Simon Goellner <simey.me@gmail.com>
2012-11-19 01:17:05 +0000

    some nice ui changes

--- js/main.js
+++ js/main.js
@@ -1,17 +1,29 @@
 
 	/* ==========================================================================
-	   Datepickers
+	   Jquery UI Defaults
 	   ========================================================================== */
 		
 		$(function() {
 			
+			
+			$.extend( $.ui.dialog.prototype.options, { width: 400, height: 160 });
+			//$.extend( $.ui.datepicker.prototype.options, { dateFormat: "yy-mm-dd", showOn: 'both', buttonImage: "img/vendor/fugue/icons/calendar-month.png", buttonImageOnly: true });
+			
+			
 			if( !Modernizr.touch ) {
 			
-				$('input[type=date]').datepicker({ dateFormat: "yy-mm-dd", showOn: 'both', buttonImage: "img/vendor/fugue/icons/calendar-month.png", buttonImageOnly: true });
+				$('input[type=date]').datepicker();
 				
 			}
+			
+			
+			
+			
 		
 		});
+		
+		
+		
 		
 	/* ==========================================================================
 	   Close Alerts
--- js/pages/gallery.js
+++ js/pages/gallery.js
@@ -2,6 +2,10 @@
 
 	$(function() {
 		
+		var gallery = {};
+		
+		
+		
 		
 		$('#button-upload').on('click', function(e) {
 
@@ -11,27 +15,65 @@
 		});
 		
 		
+		$('#button-saveorder').on('click', function() {
+			
+			var $confirm = $('#dialog-saveorder');
+			
+			$confirm.dialog({
+				
+				modal: true,
+				buttons: {
+					
+					"ok": {
+					  text:'Ok', class:'btn blue',
+					  click: function() { gallery.sendGalleryState(); $(this).dialog('close'); }
+					},
+					"cancel": {
+					  text:'Cancel', class:'btn',
+					  click: function() { $(this).dialog('close'); }
+					}
+				}
+				
+			});
+			
+			
+		});
 		
+		$('#button-resetorder').on('click', function() {
+			gallery.galleryReset();
+		});
+		
+		$('.btn.delete-image').on('click', function(e) {
+			$(this).parent('.thumbnail').attr('data-destroy','_destroy').fadeOut();
+			$('#button-saveorder, #button-resetorder').attr('disabled',false);
+			e.preventDefault();
+		});
 		
 		
 		
 		
 
-
-
+		
 
 		// make the gallery sortable
 
 		$('.gallery-wrapper').sortable({ 
+		
 			items: 			'.thumbnail',
 			placeholder: 	'placeholder',
-			forcePlaceholderSize: true
+			forcePlaceholderSize: true,
+			
+			stop: function() {
+			
+				$('#button-saveorder, #button-resetorder').attr('disabled',false);
+				
+			}
 			
 		});
 
 		// this returns the order of the gallery items.
 
-		var saveGalleryState = function() {
+		gallery.galleryState = function( set ) {
 		
 			var $gallery = $('.gallery-wrapper');
 			var stack = {};
@@ -40,16 +82,52 @@
 			
 				var $_this = $(this);
 				var id = $_this.data('id');
-				var position = $_this.index()+1;
+				
+				var position = $_this.attr('data-destroy') == '_destroy' ? '_destroy' : $_this.index()+1;
 				
 				stack[id] = position;
+			
+				if( set != "undefined" ) {
+					$(this).attr('data-order', position);	
+				}
 			
 			});
 			
 			return stack;
 		
-		}
-
+		};
+		
+		
+		gallery.galleryReset = function() {
+			
+			var howMany = $('.thumbnail').length;
+			
+			for( i=0; i<=howMany; i++ ) {
+				$('.gallery-wrapper').append( $('.gallery-wrapper .thumbnail[data-order="'+i+'"]').removeAttr('data-destroy').fadeIn() );
+			}	
+			
+		};
+		
+		//save original state of gallery
+		gallery.galleryState('set');
+		
+		
+		gallery.sendGalleryState = function() {
+			
+			$.ajax( '#' , {
+				
+				type: 'POST',
+				data: gallery.galleryState()
+				
+			});
+			
+			console.log('data posted, check network tab in console');
+			
+		};
+		
+		
+		
+		
 
 		
 		
