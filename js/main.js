a5ccac890181b99855cf36a1cb540f9e752869e7
Simon Goellner <simey.me@gmail.com>
2012-11-21 01:19:48 +0000

    meh

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
 		$(function() {
 			
 			// set defaults for jquery ui widgets
-			$.extend( $.ui.dialog.prototype.options, { width: 400, height: 160 });
+			$.extend( $.ui.dialog.prototype.options, { width: 400, height: 160, show: { effect: 'drop', direction: 'up', distance: 20, duration: 300 },  hide: { effect: 'drop', direction: 'down', distance: 20, duration: 550 } });
 			$.datepicker.setDefaults({ dateFormat: "yy-mm-dd", showOn: 'both', buttonImage: "img/vendor/fugue/icons/calendar-month.png", buttonImageOnly: true });
 			
 			// only apply datepicker to desktop devices.
