39061c4442d4d9985f43b249090b23c7d84422e7
Simon Goellner <simey.me@gmail.com>
2013-01-18 21:52:28 +0000

    re-wrote html to be less clunky

--- html-includes/dashboard.php
+++ html-includes/dashboard.php
@@ -11,79 +11,64 @@
 		
 			<div class="row-fluid">
 				
-				<div class="span4">
-					<h3><a href="?pages">
+				<div class="span12">
+				
+					<h3 class="section"><a href="?pages">
 						<img src="<?php echo( $theme ) ?>/img/vendor/fugue/icons/documents-stack.png" class="icon">
 						Content <small>Manage main areas of content on your website</small>
 					</a></h3>
-				</div>
-				
-				<div class="span4">
-					<h3><a href="?news">
+					
+					<h3 class="section"><a href="?news">
 						<img src="<?php echo( $theme ) ?>/img/vendor/fugue/icons/newspapers.png" class="icon">
 						News <small>Manage your news or blog</small>
 					</a></h3>
+					
+					<h3 class="section"><a href="?gallery">
+						<img src="<?php echo( $theme ) ?>/img/vendor/fugue/icons/photo-album.png" class="icon">
+						Galleries <small>Add and remove images or entire galleries</small>
+					</a></h3>
+					
 				</div>
-				
 				
 			</div>
 			
 			<hr>
 			
+			
+			
 			<div class="row-fluid">
 				
 				<div class="span12">
 					
-					<div class="row-fluid">
+					<h3>Custom Modules</h3>
 					
-						<div class="span4">
-							
-							<h4><a href="#">
+					<div class="custom-modules">
+					
+							<h4 class="module"><a href="#">
 								<img src="<?php echo( $theme ) ?>/img/vendor/fugue/icons/layout-hf-3.png" class="icon">
 								Panels <small>3-Column content panels</small>
 							</a></h4>
-							
-						</div>
 						
-						<div class="span4">
-							
-							<h4><a href="#">
+							<h4 class="module"><a href="#">
 								<img src="<?php echo( $theme ) ?>/img/vendor/fugue/icons/flask.png" class="icon">
 								Expressions <small>Manage your whisky expressions</small>
 							</a></h4>
 							
-						</div>
-						
-						<div class="span4">
 							
-							<h4><a href="#">
+							<h4 class="module"><a href="#">
 								<img src="<?php echo( $theme ) ?>/img/vendor/fugue/icons/pictures-stack.png" class="icon">
 								Rare Crafts <small>Manage your Rare crafts</small>
 							</a></h4>
 							
-						</div>
-					
-					</div>
-					
-					<div class="row-fluid">
-					
-						<div class="span4">
-							
-							<h4><a href="#">
+							<h4 class="module"><a href="#">
 								<img src="<?php echo( $theme ) ?>/img/vendor/fugue/icons/balloon-box.png" class="icon">
 								Comments <small>Manage Tour Comments</small>
 							</a></h4>
 							
-						</div>
-						
-						<div class="span4">
-							
-							<h4><a href="#">
+							<h4 class="module"><a href="#">
 								<img src="<?php echo( $theme ) ?>/img/vendor/fugue/icons/chain.png" class="icon">
 								Quicklinks <small>Manage Quicklinks in the footer</small>
 							</a></h4>
-							
-						</div>
 					
 					</div>
 					
@@ -95,25 +80,23 @@
 
 			<div class="row-fluid">
 				
-				<div class="span4">
-					<h3><a href="?users">
+				<div class="span12">
+				
+					<h3 class="section"><a href="?users">
 						<img src="<?php echo( $theme ) ?>/img/vendor/fugue/icons/user.png" class="icon">
 						Users <small>Manage Users and Permissions</small>
 					</a></h3>
-				</div>
 				
-				<div class="span4">
-					<h3><a href="#">
+					<h3 class="section"><a href="#">
 						<img src="<?php echo( $theme ) ?>/img/vendor/fugue/icons/book-open-list.png" class="icon">
 						Admin Log <small>View all changes made to the CMS</small>
 					</a></h4>
-				</div>
 				
-				<div class="span4">
-					<h3><a href="#">
+					<h3 class="section"><a href="#">
 						<img src="<?php echo( $theme ) ?>/img/vendor/fugue/icons/gear.png" class="icon">
 						Website Config <small>Change Global Website settings</small>
 					</a></h4>
+					
 				</div>
 				
 			</div>
