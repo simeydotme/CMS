1b0d8472fe4b3470d94ab8a8937502a13fe1dde9
Simon Goellner <simey.me@gmail.com>
2013-01-25 22:04:45 +0000

    took out spinner JS , put into tables.js

--- themes/birdchess/js/main.js
+++ themes/birdchess/js/main.js
@@ -3,38 +3,6 @@
 	window.cms = { theme: "themes/birdchess" };
 	
 	
-($(function() {
-		
-		
-	/* ==========================================================================
-	   This function shows tiny spinner in place of 'visible' in main tables.
-	   ========================================================================== */
-				
-		$('.state.active, .state.inactive').on('click', function(e) {
-			
-			e.preventDefault();
-			
-			var $this = $(this).hide();
-			var $i = $('<i class="spinner tiny"></i>');
-			
-			$this.after( $i );
-			
-			if( $this.is('.active') ) { $this.removeClass('active').addClass('inactive'); }
-			else if( $this.is('.inactive') ) { $this.removeClass('inactive').addClass('active'); }
-			
-			// timeout to simulate ajax call
-			setTimeout( function() {
-				
-				$i.fadeOut( function() { $this.fadeIn(); });
-				
-			}, 800);
-			
-		});
-		
-		
-		
-}));
-
 
 
 
