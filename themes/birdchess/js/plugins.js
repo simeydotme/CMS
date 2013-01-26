01705a5f68da98f68bb35f8063a7ac9bb54b2e6d
Simon Goellner <simey.me@gmail.com>
2013-01-26 17:51:36 +0000

    tidied up

--- themes/birdchess/js/plugins.js
+++ themes/birdchess/js/plugins.js
@@ -1,17 +1,21 @@
-// Avoid `console` errors in browsers that lack a console.
-if (!(window.console && console.log)) {
-    (function() {
-        var noop = function() {};
-        var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'markTimeline', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'];
-        var length = methods.length;
-        var console = window.console = {};
-        while (length--) {
-            console[methods[length]] = noop;
-        }
-    }());
-}
+	
+	
+	/* ==========================================================================
+	   Avoid `console` errors in browsers that lack a console.
+	   ========================================================================== */
 
-// Place any jQuery/helper plugins in here.
+		if (!(window.console && console.log)) {
+			(function() {
+				var noop = function() {};
+				var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'markTimeline', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'];
+				var length = methods.length;
+				var console = window.console = {};
+				while (length--) {
+					console[methods[length]] = noop;
+				}
+			}());
+		}
+
 
 
 
