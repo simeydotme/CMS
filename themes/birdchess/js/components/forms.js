337ed73a8be6eefc20167f4911e35fd1f48d5229
Simon Goellner <simey.me@gmail.com>
2013-02-03 14:53:27 +0000

    adding in radio/select

--- themes/birdchess/js/components/forms.js
+++ themes/birdchess/js/components/forms.js
@@ -4,18 +4,33 @@
 	  Custom Checkbox
 	   ========================================================================== */
 		
+		
+		
+		
+		$(document).ready(function() {
+			
+			$(':checkbox').chkbox();
+    		$(':radio').radio();
+			$('select').select2({ minimumResultsForSearch: 14 });
 
+		});
+		
+		
+		
 		$.fn.chkbox = function() {
 		  
 			return $(this).each( function(k,v) {
 			
 				var $this = $(v);
-				if( $this.is(':checkbox') ) {
+				if( $this.is(':checkbox') && !$this.data('checkbox-replaced') ) {
 				 	
+					// add some data to this checkbox so we can avoid re-replacing it.
+					$this.data('checkbox-replaced', true);
+					
 					// create HTML for the new checkbox.
 					var $l = $('<label for="'+$this.attr('id')+'" class="chkbox"></label>');
-					var $y = $('<span class="yes">&#10003;</span>');
-					var $n = $('<span class="no">&#10060;</span>');
+					var $y = $('<span class="yes">checked</span>');
+					var $n = $('<span class="no">unchecked</span>');
 					var $t = $('<span class="toggle"></span>');
 					
 					// insert the HTML in before the checkbox.
@@ -46,7 +61,66 @@
 			
 			});
 	
-		}; 
-		$(':checkbox').chkbox();
+		};
 		
+		
+		
+		
+		
+		$.fn.radio = function() {
+		  
+			return $(this).each( function(k,v) {
+			  
+				var $this = $(v);
+				if( $this.is(':radio') && !$this.data('radio-replaced') ) {
+				 	
+					// add some data to this checkbox so we can avoid re-replacing it.
+					$this.data('radio-replaced', true);
+					
+					// create HTML for the new checkbox.
+					var $l = $('<label for="'+$this.attr('id')+'" class="radio"></label>');
+					var $p = $('<span class="pip"></span>');
+					
+					// insert the HTML in before the checkbox.
+					$l.append( $p ).insertBefore( $this );
+					$this.addClass('replaced');
+					
+					// check if the radio is checked, apply styling. trigger focus.
+					$this.on('change', function() {
+            
+						$('label.radio').each( function(k,v) {
+						
+							var $v = $(v);
+							if( $('#'+ $v.attr('for') ).is(':checked') ) {
+								$v.addClass('on'); } 
+							else { $v.removeClass('on'); }
+						
+						});
+            
+						$this.trigger('focus');
+					
+					});
+					
+					$this.on('focus', function() { $l.addClass('focus') });
+					$this.on('blur', function() { $l.removeClass('focus') });
+					
+					
+					
+					
+					// check if the radio is checked on init.
+					$('label.radio').each( function(k,v) {
+              
+						var $v = $(v);
+						if( $('#'+ $v.attr('for') ).is(':checked') ) {
+							$v.addClass('on'); } 
+						else { $v.removeClass('on'); }
+				
+					});
+				  
+				}
+			
+			});
+	
+		}; 
+
 
